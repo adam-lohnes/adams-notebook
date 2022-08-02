b5a47c9a008eafe936db8b7ed95668da6ff52ced
Adam <adam.a.lohnes@gmail.com>
2022-08-01 19:23:08 -0700

    init: create initial landing page

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Adam's Notebook</h1>
+      </header>
+      <main className="App-body">
+        <h2>Hello World!</h2>
+        <h3>2022-08-01</h3>
+        <p>
+          This is my first notebook entry.
+        </p>
+        <p>
+          I'm actually hard-coding this entry intially, though in the near future, I'm planning to have a remote CMS spun up to handle the actual running content here.
+        </p>
+        <p>
+          As the title above indicates, this it just a way for me to actually get out of the infinite cycle of planning and preparing I've been stuck in, and get this project live.
+        </p>
+        <p>
+          It's not pretty, or complex at the moment, but here it is.
+        </p>
+        <p>
+          Hello World! We're going to create some amazing things!
+        </p>
+        <h4>>>>Adam</h4>
+      </main>
+    </div>
+  );
+}
+
+export default App;
