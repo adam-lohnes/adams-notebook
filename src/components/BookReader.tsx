09159714a37a7f15bad9c2036dc8b8d21bd6e863
Adam Lohnes <adamalohnes@gmail.com>
2025-03-31 16:40:01 -0700

    fix: restore useEffect + dependency

--- src/components/BookReader.tsx
+++ src/components/BookReader.tsx
@@ -64,6 +64,13 @@ export default function BookReader({ project, chapter, prevChapterNum, nextChapt
     }
   };
 
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [prevChapterNum, nextChapterNum, handleKeyDown]);
+
   const toggleTableOfContents = () => {
     setShowToc(!showToc);
   };
