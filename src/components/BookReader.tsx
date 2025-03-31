8524f49c88cc327a0336b0ae358d7e8114f4f093
Adam Lohnes <adamalohnes@gmail.com>
2025-03-31 16:38:34 -0700

    fix: unused useEffect

--- src/components/BookReader.tsx
+++ src/components/BookReader.tsx
@@ -64,13 +64,6 @@ export default function BookReader({ project, chapter, prevChapterNum, nextChapt
     }
   };
 
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [prevChapterNum, nextChapterNum]);
-
   const toggleTableOfContents = () => {
     setShowToc(!showToc);
   };
