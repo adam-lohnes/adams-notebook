419762867bd7659ed0d20daabbd61b33c1221659
Adam Lohnes <adamalohnes@gmail.com>
2025-04-02 17:20:09 -0700

    chore: Google Analytics only in production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
-        <GoogleAnalytics />
+        {process.env.NODE_ENV === 'production' && <GoogleAnalytics />}
       </head>
       <body className={`${inter.className} flex flex-col min-h-screen bg-gray-50 dark:bg-gray-950 text-gray-900 dark:text-gray-100 antialiased`}>
         <Header />
